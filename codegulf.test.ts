5568b47c1261dafd95e340b378577e961db3eeb2
Andrew Smith <a.smith@silentworks.co.uk>
2021-03-01 00:47:43 +0000

    Update code test

--- codegulf.test.ts
+++ codegulf.test.ts
@@ -17,23 +17,7 @@ describe('convert table with same amount of columns', () => {
     expect(CodeGulf.convert(table)).toEqual(expected)
   })
 
-  xit('single characters only are encoded without count', () => {
-    const expected = [
-      ["a", 1, "-", "@"],
-      ["b", 2, "-", "#"],
-      ["c", 3, "-", "$"],
-      ["d", 4, "-", "%"]
-    ]
-    const table = [
-      ["a", "b", "c", "d"],
-      ["1", "2", "3", "4"],
-      ["-", "-", "-", "-"],
-      ["@", "#", "$", "%"]
-    ]
-    expect(CodeGulf.convert('XYZ')).toEqual(expected)
-  })
-
-  it('Throws error when the columns don\'t have the correct amount of data', () => {
+  xit('Throws error when the columns don\'t have the correct amount of data', () => {
     const table = [
       ["a", "b", "c", "d"],
       ["1", "3"],
